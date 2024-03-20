0110554277b16ce260662da3d839b5887e28993a
Yauhen <yushkevicheugen@gmail.com>
2024-03-20 15:02:40 +0100

    Add function for char list

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -24,6 +24,7 @@ class MarvelService {
 
     _transformCharacter = (char) =>{
         return {
+            id: char.id,
             name: char.name,
             description: char.description ?  `${char.description.slice(0, 210)}...` : "There is no description",
             thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
